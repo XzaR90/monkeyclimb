e282a05fc6664702e2ce1880666bd1207912564d
Josh Gomez <josh.gomez90@gmail.com>
2014-10-07 15:25:12 +0200

    Generated Visual Studio files, added HealthController. (2)

---
+++ Unity/MonkeyClimb/Assets/Scripts/Controllers/HealthController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+public class HealthController : MonoBehaviour
+{
+	/// <summary>
+	/// Total hitpoints
+	/// </summary>
+	public int hp = 1;
+	
+	/// <summary>
+	/// Enemy or player?
+	/// </summary>
+	public bool isEnemy = true;
+	
+	/// <summary>
+	/// Inflicts damage and check if the object should be destroyed
+	/// </summary>
+	/// <param name="damageCount"></param>
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+		if (this.hp <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+
+	}
+}
