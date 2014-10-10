49221d0ebe3b2362ffc06306fc6720f8de2f1b3f
Anders Jansson <nowevenbetter@gmail.com>
2014-10-10 12:19:43 +0200

    Added image to level selector scene

--- Unity/MonkeyClimb/Assets/Scripts/Controllers/Menus/LevelMenuController.cs
+++ Unity/MonkeyClimb/Assets/Scripts/Controllers/Menus/LevelMenuController.cs
@@ -5,9 +5,25 @@ namespace Controllers.Menus
 {
 	public class LevelMenuController : MonoBehaviour {
 
-		// Use this for initialization
-		void Start () {
-		
+		public Texture btnTexture;
+
+		void OnGUI() {
+			float buttonWidth = Screen.width;
+			float buttonHeight = (Screen.height/3);
+
+			Rect levelButton = new Rect(0,0,buttonWidth,buttonHeight);
+
+			// Draw a button to start the game
+			if(GUI.Button(levelButton,btnTexture))
+			{
+				Debug.Log("Clicked the button with an image");
+				//Application.LoadLevel("SinglePlayer");
+			}
+			/*
+			if (!btnTexture) {
+				Debug.LogError("Please assign a texture on the inspector");
+				return;
+			}*/
 		}
 		
 		// Update is called once per frame
